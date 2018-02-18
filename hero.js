db0f68cd0ece20cafe4780c0e31cb350dd1c82dd
Graeme Brown <graemehbrown@googlemail.com>
2018-02-18 13:57:50 +0000

    Added Hero class with constructor and talk method.

--- hero.js
+++ hero.js
@@ -1,3 +1,14 @@
 // hero
 
-module.exports = hero
+const Hero = function (name, favouriteFood) {
+    this.name = name;
+    this.health = 0;
+    this.favouriteFood= favouriteFood; 
+    this.tasks = [];   
+}
+
+Hero.prototype.talk = function() {
+    return 'My name is: '+this.name;    
+}
+
+module.exports = Hero
--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -1,2 +1,34 @@
 const assert = require('assert')
-const hero = require('../hero.js')
+const Hero = require('../hero.js')
+
+describe('Hero', function() {
+    let hero;
+    beforeEach(function() {
+        hero = new Hero('Guybrush Threepwood', 'Carrot cake')
+    });
+
+    it('should have a name' , function () {
+        const actual = hero.name;
+        assert.strictEqual(actual, 'Guybrush Threepwood');
+    });
+
+    it('should have a favourite food', function() {
+        const actual = hero.favouriteFood;
+        assert.strictEqual(actual, 'Carrot cake');
+    });
+    
+    it('should have a health level', function() {
+        const actual = hero.health;
+        assert.strictEqual(actual, 0);
+    });
+
+    it('can talk', function() {
+        const actual = hero.talk();
+        assert.strictEqual(actual, 'My name is: '+ hero.name);
+    });
+
+    it('has a collection of tasks to complete', function() {
+        const actual = hero.tasks;
+        assert.deepStrictEqual(actual, []);
+    })
+});
